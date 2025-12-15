b1ad651bcdf587e00d0249fadf48a0e0caebd51b
HexaQuebec <hexaquebec80@gmail.com>
2025-12-15 06:47:42 +0100

    Add executable start.sh for Railway

--- hexaquebec/start.sh
+++ hexaquebec/start.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-set -e
-
+# Définit le fichier de configuration Django
 export DJANGO_SETTINGS_MODULE=hexaquebec.settings
+export PYTHONUNBUFFERED=1
 
+# Collecte les fichiers statiques
 python manage.py collectstatic --noinput
+
+# Applique les migrations de la base de données
 python manage.py migrate
 
+# Démarre le serveur Gunicorn
 gunicorn hexaquebec.wsgi:application --bind 0.0.0.0:$PORT
