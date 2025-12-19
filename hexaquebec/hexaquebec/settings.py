6d244cfbc6988a17bc30d9d62cea8944b0da1dc2
HexaQuebec <hexaquebec80@gmail.com>
2025-12-19 03:03:17 +0100

    Update email sending and client profile

--- hexaquebec/hexaquebec/settings.py
+++ hexaquebec/hexaquebec/settings.py
@@ -115,8 +115,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv("EMAIL_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASSWORD")
+EMAIL_HOST_USER = 'hexaquebec80@gmail.com'
+EMAIL_HOST_PASSWORD = 'f x b n j i t h o p v d p n s p'
+
 
 # ========================
 # CLOUDINARY (POUR RENDER)
