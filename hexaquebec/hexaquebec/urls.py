0197a0a9736a4d4599526b3a2f26046dbe048554
Diambere2001 <hexaquebec80@gmail.com>
2026-07-11 21:51:14 -0400

    Correction section stage desktop tablette

--- hexaquebec/hexaquebec/urls.py
+++ hexaquebec/hexaquebec/urls.py
@@ -16,15 +16,26 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 
 urlpatterns = [
+
+    path(
+        "favicon.ico",
+        RedirectView.as_view(
+            url="/static/images/logoHexa.png",
+            permanent=True
+        ),
+    ),
+
     path('admin/', admin.site.urls),
-    path('', include('siteweb.urls')),  # Application principale
-   
+
+    path('', include('siteweb.urls')),
+
 ]
 
 # Servir les fichiers médias (images) en développement
